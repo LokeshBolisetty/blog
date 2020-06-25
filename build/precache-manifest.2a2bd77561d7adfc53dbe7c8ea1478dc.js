0fe5797aaee8e7c160a0af7ae55cdbeff4d7f735
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-25 16:44:09 +0530

    Add files via upload

---
+++ build/precache-manifest.2a2bd77561d7adfc53dbe7c8ea1478dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba06118e3f7f427321d3033a89024b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dafab27f78113823d9e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "098d309f3a650b21a89a",
+    "url": "/static/css/main.b3b0b088.chunk.css"
+  },
+  {
+    "revision": "0dafab27f78113823d9e",
+    "url": "/static/js/2.92f7bee0.chunk.js"
+  },
+  {
+    "revision": "20451c1d098c44474cfe49e2f962af34",
+    "url": "/static/js/2.92f7bee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098d309f3a650b21a89a",
+    "url": "/static/js/main.7fee0436.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "fcb3809bac6c7d2deee609933766749c",
+    "url": "/static/media/Wood Background.fcb3809b.jfif"
+  },
+  {
+    "revision": "3c9b5c3fc08781c531a79ab8ab6ad747",
+    "url": "/static/media/search image.3c9b5c3f.png"
+  }
+]);
